a78ec1b1f8243f882113fd9f3bf58f079da859db
Aiswarya Sarath <49971693+aishwaryasarath@users.noreply.github.com>
2022-05-27 12:56:48 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,21 +1,34 @@
 variable "region" {
-    type = string
-    description = "The region to deploy resources to"
-    default = "us-east-1"
+  type        = string
+  description = "The region to deploy resources to"
+  default     = "us-east-1"
 }
 
 
 variable "vpc_cidr" {
-    type = string
-    description = "CIDR block for VPC"
-    default = "10.255.0.0/20"
+  type        = string
+  description = "CIDR block for VPC"
+  default     = "10.255.0.0/20"
 }
 
 variable "default_tags" {
-    type = map(string)
-    description = "Map of default tags to apply to resources"
-    default = {
-      project = "learning live with AWS & HashiCorp"
+  type        = map(string)
+  description = "Map of default tags to apply to resources"
+  default = {
+    project = "learning live with AWS & HashiCorp"
 
-    }
+  }
+}
+
+variable "vpc_public_subnet_count" {
+  type        = number
+  description = "Number of public subnets to create"
+  default     = 2
+}
+
+
+variable "vpc_private_subnet_count" {
+  type        = number
+  description = "Number of private subnets to create"
+  default     = 2
 }
